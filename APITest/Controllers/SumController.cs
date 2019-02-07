49e5a62bd7402802b5e0514b0a1f6757431c5c38
spardadanthe <stanimir.venkov@eldersoss.com>
2019-02-07 16:31:53 +0200

    Sum API created

---
+++ APITest/Controllers/SumController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APITest.Controllers
+{
+    public class SumController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(string num1, string num2)
+        {
+            int number = int.Parse(num1);
+            int number2 = int.Parse(num2);
+
+            return (number + number2).ToString();
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
